2d8e41ac6a9a5e2bfd408797497b2dc50a35a097
khizar <khizarwahab19@gmail.com>
2025-02-16 23:16:49 +0500

    Add delete many to users

--- resources/js/Pages/Admin/Users/Index.tsx
+++ resources/js/Pages/Admin/Users/Index.tsx
@@ -6,12 +6,14 @@ import CreateEditModal from './Components/CreateEditModal';
 import DeleteModal from '../../../Components/Common/DeleteModal';
 import DataTable, { ColumnDef } from '../../../Components/DataTable';
 import BreadCrumb2 from '../../../Components/Common/BreadCrumb2';
+import DeleteManyModal from '../../../Components/Common/DeleteManyModal';
 
 function Index({ users }: any) {
     const [showCreateEditModal, _setShowCreateEditModal] = React.useState(false);
     const [editUser, setEditUser] = React.useState<any>(null);
     const [deleteUser, setDeleteUser] = React.useState<any>(null);
     const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+    const [showDeleteManyConfirmation, setShowDeleteManyConfirmation] = useState(false);
 
     const setShowCreateEditModal = (state: boolean) => {
         _setShowCreateEditModal(state);
@@ -22,6 +24,11 @@ function Index({ users }: any) {
 
     const deleteForm = useForm({
         _method: 'DELETE'
+    });
+
+    const deleteManyForm = useForm<{ _method: string; ids: number[] }>({
+        _method: 'DELETE',
+        ids: [],
     });
 
     const editAction = (user: any) => {
@@ -37,6 +44,16 @@ function Index({ users }: any) {
     const handleDelete = () => {
         deleteForm.delete(route('admin.users.destroy', deleteUser.id));
         setShowDeleteConfirmation(false);
+    }
+
+    const deleteManyAction = (ids: number[]) => {
+        deleteManyForm.setData(data => ({...data, ids: ids}));
+        setShowDeleteManyConfirmation(true);
+    }
+
+    const handleDeleteMany = () => {
+        deleteManyForm.delete(route('admin.users.destroy.many'));
+        setShowDeleteManyConfirmation(false);
     }
 
     const columns: ColumnDef<typeof users.data[0]> = [
@@ -85,9 +102,16 @@ function Index({ users }: any) {
                                 columns={columns}
                                 title="Users"
                                 actions={[
+                                    // Delete multiple
                                     {
-                                        render: <Button onClick={() => setShowCreateEditModal(true)}>Add New</Button>
-                                    }
+                                        render: (dataTable) => <Button className="btn-danger" onClick={() => deleteManyAction(dataTable.getSelectedRows().map(row => row.id))}><i className="ri-delete-bin-5-line"></i> Delete ({dataTable.getSelectedRows().length})</Button>,
+                                        showOnRowSelection: true,
+                                    },
+
+                                    // Add new
+                                    {
+                                        render: <Button onClick={() => setShowCreateEditModal(true)}><i className="ri-add-fill"></i> Add New</Button>
+                                    },
                                 ]}
                             />
                         </Col>
@@ -108,6 +132,12 @@ function Index({ users }: any) {
                 show={showDeleteConfirmation}
                 onDeleteClick={handleDelete}
                 onCloseClick={() => { setShowDeleteConfirmation(false) }}
+            />
+
+            <DeleteManyModal
+                show={showDeleteManyConfirmation}
+                onDeleteClick={handleDeleteMany}
+                onCloseClick={() => { setShowDeleteManyConfirmation(false) }}
             />
         </React.Fragment>
     )
--- routes/admin.php
+++ routes/admin.php
@@ -8,5 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('users', UserController::class);
+    Route::delete('users/delete/many', [UserController::class, 'destroyMany'])->name('users.destroy.many');
     Route::resource('roles', RoleController::class);
 });
