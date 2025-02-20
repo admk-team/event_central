2c6e1044cad8d6b247e034c734d1b70bff206942
Nahoor07 <nahoor98@gmail.com>
2025-02-20 15:01:20 +0500

    added event defaut layout

--- resources/js/Layouts/Organizer/index.tsx
+++ resources/js/Layouts/Organizer/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import OrganizerProfileDropdown from '../../Components/Common/OrganizerProfileDropdown'
 
 export default function Index({children} : any) {
   return (
     <React.Fragment>
         <div id="layout-wrapper">
+          <header className="page-topbar border-bottom d-flex justify-content-end">
+            <OrganizerProfileDropdown></OrganizerProfileDropdown>
+          </header>
             <div className="main-content">
                 {children}
             </div>
