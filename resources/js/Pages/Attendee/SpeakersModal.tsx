d771402558e5340bfaad771628f92628b1a97313
ansar <ansar.mcs2009@gmail.com>
2025-04-09 15:23:53 +0500

    attendee speaker page fixes

--- resources/js/Pages/Attendee/SpeakersModal.tsx
+++ resources/js/Pages/Attendee/SpeakersModal.tsx
@@ -1,10 +1,12 @@
-import { useForm, usePage, router } from '@inertiajs/react';
+import { Link, router } from '@inertiajs/react';
 import Flatpickr from "react-flatpickr";
 import { Spinner, Col, Modal, Row, Tab, Button } from 'react-bootstrap';
 import speakerAvatar from '../../../images/speaker_avatar.svg';
 
 
 export default function SpeakerModal({ show, hide, onHide, event, speaker }: { show: boolean, hide: () => void, onHide: () => void, event: any, speaker: any }) {
+
+    // console.log(speaker);
 
     const gotoEventSpeaker = () => {
         router.visit(route('attendee.event.detail.speakers', [speaker.id]));
@@ -18,8 +20,10 @@ export default function SpeakerModal({ show, hide, onHide, event, speaker }: { s
                         <Button className='btn-sm' variant='outline-secondary' onClick={gotoEventSpeaker}>See All Speakers</Button>
                     </Col>
                     <Col className='p-4 flex-column d-flex justify-content-center align-items-center'>
-                        <img src={speakerAvatar} alt="speaker Avatar" style={{ height: '150ps', borderRadius: '50%', marginBottom: '15px' }} />
-                        <h5>{speaker.name}</h5>
+                        <Link href={route('attendee.event.detail.speakers', speaker.id)}>
+                            <img src={speaker.avatar || speakerAvatar} alt="speaker Avatar" className='rounded-circle avatar-xl' width="150" />
+                            <h5 className='m-3'>{speaker.name}</h5>
+                        </Link>
                     </Col>
                 </Row>
             </Modal.Body>
