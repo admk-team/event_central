cf146135b9ff8293b4426e33b86b33a3b3490605
Sibi haseb <abdul.haseeb.ali@gmail.com>
2025-04-11 00:26:50 +0500

    Redsign Ticket for marty

--- app/Http/Controllers/Attendee/Payment/PaymentController.php
+++ app/Http/Controllers/Attendee/Payment/PaymentController.php
@@ -378,6 +378,7 @@ class PaymentController extends Controller
                     'qr_code' => asset('storage/' . $purchasedTicket->qr_code),
                     'purchased_id' => $purchasedTicket->id,
                     'transfer_check' => $transferCheck,
+                    'ticket_name' => $purchasedTicket->ticket->name,
                 ];
             }
         }
