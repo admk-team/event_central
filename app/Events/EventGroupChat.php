bf86f2b68cebc616c8c30b4f119dbf5dcaa65552
FaiqSarwar-2669 <code.faiq786@gmail.com>
2025-07-21 18:07:43 +0500

    chat in progress

---
+++ app/Events/EventGroupChat.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\EventApp;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class EventGroupChat
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public $event;
+
+    public function __construct(EventApp $event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('event-app-' . $this->event),
+        ];
+    }
+}
