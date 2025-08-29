af8ace50161976de8b0c89335007a9f41da01670
mohammadatif786 <mohammadatif24680@gmail.com>
2025-08-29 12:40:39 +0500

    send addons and extra service to attendee and orginzer email

--- resources/js/Layouts/Organizer/LayoutMenuData.tsx
+++ resources/js/Layouts/Organizer/LayoutMenuData.tsx
@@ -169,22 +169,22 @@ const Navdata = () => {
                 },
             ],
         },
-        {
-            id: "liveStreamSettings",
-            label: "Live Stream Settings",
-            icon: "bx bx-broadcast",
-            link: route('organizer.settings.live-stream.index'),
-            stateVariables: isLiveStreamSettings,
-            click: function (e: any) {
-                e.preventDefault();
-                setIsDashboard(!isLiveStreamSettings);
-                setIscurrentState('LiveStreamSettings');
-                updateIconSidebar(e);
-            },
-            hasPermissions: [
-                'edit_live_stream_settings',
-            ]
-        },
+        // {
+        //     id: "liveStreamSettings",
+        //     label: "Live Stream Settings",
+        //     icon: "bx bx-broadcast",
+        //     link: route('organizer.settings.live-stream.index'),
+        //     stateVariables: isLiveStreamSettings,
+        //     click: function (e: any) {
+        //         e.preventDefault();
+        //         setIsDashboard(!isLiveStreamSettings);
+        //         setIscurrentState('LiveStreamSettings');
+        //         updateIconSidebar(e);
+        //     },
+        //     hasPermissions: [
+        //         'edit_live_stream_settings',
+        //     ]
+        // },
     ];
     return <React.Fragment>{menuItems}</React.Fragment>;
 };
