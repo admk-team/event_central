de7bc3c2802ee05cb65eef35f22339ca5082a972
manazir77 <manazirhussain177@gmail.com>
2025-09-02 12:13:03 +0500

    c7

--- resources/js/Pages/Admin/Dashboard/Widget.tsx
+++ resources/js/Pages/Admin/Dashboard/Widget.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import CountUp from "react-countup";
-
-//Import Icons
 import FeatherIcon from "feather-icons-react";
+import { useLaravelReactI18n } from "laravel-react-i18n";
 
 const Widget = () => {
+    const { t } = useLaravelReactI18n();
+
     return (
         <React.Fragment>
             <Row>
@@ -14,27 +15,22 @@ const Widget = () => {
                         <Card.Body>
                             <div className="d-flex justify-content-between">
                                 <div>
-                                    <p className="fw-medium text-muted mb-0">Users</p>
+                                    <p className="fw-medium text-muted mb-0">{t("users")}</p>
                                     <h2 className="mt-4 ff-secondary fw-semibold">
                                         <span className="counter-value">
-                                            <CountUp
-                                                start={0}
-                                                end={28.05}
-                                                decimals={1}
-                                                duration={4}
-                                            />
-                                            0</span>k</h2>
-                                    <p className="mb-0 text-muted text-truncate "><span className="badge bg-light text-success mb-0">
-                                        <i className="ri-arrow-up-line align-middle"></i> 16.24 %
-                                    </span> vs. previous month</p>
+                                            <CountUp start={0} end={28.05} decimals={1} duration={4} />
+                                        </span>k
+                                    </h2>
+                                    <p className="mb-0 text-muted text-truncate">
+                                        <span className="badge bg-light text-success mb-0">
+                                            <i className="ri-arrow-up-line align-middle"></i> 16.24 %
+                                        </span> {t("vs_previous_month")}
+                                    </p>
                                 </div>
                                 <div>
                                     <div className="avatar-sm flex-shrink-0">
                                         <span className="avatar-title bg-info-subtle rounded-circle fs-2">
-                                            <FeatherIcon
-                                                icon="users"
-                                                className="text-info"
-                                            />
+                                            <FeatherIcon icon="users" className="text-info" />
                                         </span>
                                     </div>
                                 </div>
@@ -48,27 +44,22 @@ const Widget = () => {
                         <Card.Body>
                             <div className="d-flex justify-content-between">
                                 <div>
-                                    <p className="fw-medium text-muted mb-0">Sessions</p>
+                                    <p className="fw-medium text-muted mb-0">{t("sessions")}</p>
                                     <h2 className="fs-22 mt-4 ff-secondary fw-semibold">
-                                        <span className="counter-value" data-target="97.66">
-                                            <CountUp
-                                                start={0}
-                                                end={97.66}
-                                                decimals={2}
-                                                duration={4}
-                                            />
-                                        </span>k</h2>
-                                    <p className="mb-0 text-muted text-truncate "><span className="badge bg-light text-danger mb-0">
-                                        <i className="ri-arrow-down-line align-middle"></i> 3.96 %
-                                    </span> vs. previous month</p>
+                                        <span className="counter-value">
+                                            <CountUp start={0} end={97.66} decimals={2} duration={4} />
+                                        </span>k
+                                    </h2>
+                                    <p className="mb-0 text-muted text-truncate">
+                                        <span className="badge bg-light text-danger mb-0">
+                                            <i className="ri-arrow-down-line align-middle"></i> 3.96 %
+                                        </span> {t("vs_previous_month")}
+                                    </p>
                                 </div>
                                 <div>
                                     <div className="avatar-sm flex-shrink-0">
                                         <span className="avatar-title bg-info-subtle rounded-circle fs-2">
-                                            <FeatherIcon
-                                                icon="activity"
-                                                className="text-info"
-                                            />
+                                            <FeatherIcon icon="activity" className="text-info" />
                                         </span>
                                     </div>
                                 </div>
@@ -84,33 +75,21 @@ const Widget = () => {
                         <Card.Body>
                             <div className="d-flex justify-content-between">
                                 <div>
-                                    <p className="fw-medium text-muted mb-0">Avg. Visit Duration</p>
+                                    <p className="fw-medium text-muted mb-0">{t("avg_visit_duration")}</p>
                                     <h2 className="fs-22 mt-4 ff-secondary fw-semibold">
-                                        <span className="counter-value" data-target="3">
-                                            <CountUp
-                                                start={0}
-                                                end={3}
-                                                duration={4}
-                                            />
-                                        </span>m{" "}
-                                        <span className="counter-value" data-target="40">
-                                            <CountUp
-                                                start={0}
-                                                end={40}
-                                                duration={4}
-                                            />
-                                        </span>sec</h2>
-                                    <p className="mb-0 text-muted text-truncate "><span className="badge bg-light text-danger mb-0">
-                                        <i className="ri-arrow-down-line align-middle"></i> 0.24 %
-                                    </span> vs. previous month</p>
+                                        <span className="counter-value"><CountUp start={0} end={3} duration={4} /></span> {t("min")}{" "}
+                                        <span className="counter-value"><CountUp start={0} end={40} duration={4} /></span> {t("sec")}
+                                    </h2>
+                                    <p className="mb-0 text-muted text-truncate">
+                                        <span className="badge bg-light text-danger mb-0">
+                                            <i className="ri-arrow-down-line align-middle"></i> 0.24 %
+                                        </span> {t("vs_previous_month")}
+                                    </p>
                                 </div>
                                 <div>
                                     <div className="avatar-sm flex-shrink-0">
                                         <span className="avatar-title bg-info-subtle rounded-circle fs-2">
-                                            <FeatherIcon
-                                                icon="clock"
-                                                className="text-info"
-                                            />
+                                            <FeatherIcon icon="clock" className="text-info" />
                                         </span>
                                     </div>
                                 </div>
@@ -124,27 +103,20 @@ const Widget = () => {
                         <Card.Body>
                             <div className="d-flex justify-content-between">
                                 <div>
-                                    <p className="fw-medium text-muted mb-0">Bounce Rate</p>
+                                    <p className="fw-medium text-muted mb-0">{t("bounce_rate")}</p>
                                     <h2 className="fs-22 mt-4 ff-secondary fw-semibold">
-                                        <span className="counter-value" data-target="33.48">
-                                            <CountUp
-                                                start={0}
-                                                end={33.48}
-                                                decimals={2}
-                                                duration={4}
-                                            />
-                                        </span>%</h2>
-                                    <p className="mb-0 text-muted text-truncate "><span className="badge bg-light text-success mb-0">
-                                        <i className="ri-arrow-up-line align-middle"></i> 7.05 %
-                                    </span> vs. previous month</p>
+                                        <span className="counter-value"><CountUp start={0} end={33.48} decimals={2} duration={4} /></span>%
+                                    </h2>
+                                    <p className="mb-0 text-muted text-truncate">
+                                        <span className="badge bg-light text-success mb-0">
+                                            <i className="ri-arrow-up-line align-middle"></i> 7.05 %
+                                        </span> {t("vs_previous_month")}
+                                    </p>
                                 </div>
                                 <div>
                                     <div className="avatar-sm flex-shrink-0">
                                         <span className="avatar-title bg-info-subtle rounded-circle fs-2">
-                                            <FeatherIcon
-                                                icon="external-link"
-                                                className="text-info"
-                                            />
+                                            <FeatherIcon icon="external-link" className="text-info" />
                                         </span>
                                     </div>
                                 </div>
